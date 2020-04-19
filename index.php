534dbc43442942633a1e555df4cda1720c3cac52
Phil KREJCI <kphil@protonmail.com>
2020-04-19 14:22:37 +0200

    restructure

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php
 
-    include '/Controllers/studentController';
 
-    $top = file_get_contents('Views/top.html');
+    $top = file_get_contents('views/top.html');
     if( $_SESSION["stype"] == "eleve" )
       { $top = str_replace("#0984e3","#27ae60",$top); }
     if( $_SESSION["stype"] == "helper" )
       { $top = str_replace("#0984e3","#d63031",$top); }
     echo $top;
+
+	include 'views/homepage.php';
 
 ?>
--- views/homepage.php
+++ views/homepage.php
@@ -5,11 +5,12 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
     </head>
 
     <body>
-        
+		<div>
+			<input type=text />
+		</div> 
     </body>
     
 </html>
