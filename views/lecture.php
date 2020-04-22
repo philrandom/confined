b272619b04ca4c2fd10f798c6af673e14361a02e
Phil KREJCI <kphil@protonmail.com>
2020-04-22 20:10:07 +0200

    code-prettyfier

--- views/lecture.php
+++ views/lecture.php
@@ -6,5 +6,8 @@ $z = new dispatcher("./data",str_replace("/lecture/","",$_SERVER['REQUEST_URI'])
 	<link rel="stylesheet" href="/views/style/lecture.css">
 	<meta charset="UTF-8">
 </head>
+<body>
+<script src="https://cdn.jsdelivr.net/gh/google/code-prettify@master/loader/run_prettify.js?skin=doxy"></script>
 
-<?php echo $z->read_from_file(); ?>
+<?php echo str_replace("<pre><code class=\"","<pre style=\"background-color: #0f0f0f\"><code class=\"prettyprint linenums ",$z->read_from_file()); ?>
+</body>
