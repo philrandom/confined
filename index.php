845eb9252b0ec84c33fcbe6c7d5e0c5a57e17a81
Phil KREJCI <kphil@protonmail.com>
2020-04-23 16:33:58 +0200

    correct colors

--- index.php
+++ index.php
@@ -10,10 +10,10 @@ require('./modules/file_dispatcher/main.php');?>
 	session_start();
     $top = file_get_contents('views/top.html');
     if( $_SESSION["stype"] == "eleve" )
-      { $top = str_replace("#0984e3","#27ae60",$top); }
+      { $top = str_replace("#2aa3b7","#27ae60",$top); }
     if( $_SESSION["stype"] == "helper" )
-      { $top = str_replace("#0984e3","#d63031",$top); }
-    //echo $top;
+      { $top = str_replace("#2aa3b7","#d63031",$top); }
+    echo $top;
 
 
 	$url = explode("/",$_SERVER['REQUEST_URI']);
--- views/tag.php
+++ views/tag.php
@@ -1,18 +1,19 @@
 <head>
 	<link rel="stylesheet" href="/views/style/tag_tree.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<meta charset="UTF-8">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script>
 $(document).ready(function(){
   $("#tree-tag").click(function(){
-  	if( document.getElementById ("tree-tag").innerHTML == "#tree" ) {
-  		document.getElementById ("tree-tag").innerHTML = "#tag";
+  	if( document.getElementById ("tree-tag").innerHTML == "<i class=\"fa fa-eye\"></i>tree" ) {
+  		document.getElementById ("tree-tag").innerHTML = "<i class=\"fa fa-eye\"></i>tag";
 	  	$(".shift-r").removeClass("grow-r");
 		$(".shift-r").addClass("dead-r");
 		$(".shift-l").removeClass("grow-l");
 		$(".shift-l").addClass("dead-l");
-    } else if( document.getElementById ("tree-tag").innerHTML == "#tag" ) {
-    	document.getElementById ("tree-tag").innerHTML = "#tree";
+    } else if( document.getElementById ("tree-tag").innerHTML == "<i class=\"fa fa-eye\"></i>tag" ) {
+    	document.getElementById ("tree-tag").innerHTML = "<i class=\"fa fa-eye\"></i>tree";
     	$(".shift-r").addClass("grow-r");
 		$(".shift-r").removeClass("dead-r");
 		$(".shift-l").addClass("grow-l");
@@ -32,7 +33,7 @@ foreach($z->get_tree() as $leaf) {
 }
 ?>
 </div>
-<div id='tree-tag'>#tree</div>
+<div id='tree-tag'><i class="fa fa-eye"></i>tree</div>
 
 
 </body>
