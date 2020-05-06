027278e554346f65ca3743482c5991be00470017
Phil KREJCI <kphil@protonmail.com>
2020-05-06 18:53:10 +0200

    ajax qcm_upload

--- ajax/qcm_upload.php
+++ ajax/qcm_upload.php
@@ -6,10 +6,8 @@ require('../modules/file_dispatcher/main.php');?>
 
 
 <?php
-	$z = new dispatcher("../data","282c6b88c417597f3a868b9b1d05b9ce",'u');
-	//$z = new dispatcher("../data",$_POST['h_code'],'u');
-	//if( $z->add_row_qcm($_POST['qu'],$_POST['A'],$_POST['B'],$_POST['C'],$_POST['D'],$_POST['V']) )
-	if( $z->add_row_qcm("comment ?",'ok','re','ici','la','D') )
+	$z = new dispatcher("../data",$_POST['h_code'],'u');
+	if( $z->add_row_qcm($_POST['qu'],$_POST['A'],$_POST['B'],$_POST['C'],$_POST['D'],$_POST['V']) )
 		echo "#6eae1e";
 	else
 		echo "#f90000";
